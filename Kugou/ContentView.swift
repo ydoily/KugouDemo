87556d6836a24023d8c6bee9e3c77feaefeff3da
ydoily <algoydoily@gmail.com>
2024-12-31 10:21:21 +0800

    Initial Commit

---
+++ Kugou/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kugou
+//
+//  Created by ydoily  on 12/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kugou/KugouApp.swift
@@ -0,0 +1,17 @@
+//
+//  KugouApp.swift
+//  Kugou
+//
+//  Created by ydoily  on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct KugouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
